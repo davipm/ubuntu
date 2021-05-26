9f796c8922a15aee291d66f9ec175c6335316577
davi <davi.p.m94@gmail.com>
2021-05-25 21:00:17 -0300

    adding brave install config

--- install.sh
+++ install.sh
@@ -39,9 +39,22 @@ node -v
 
 ## Browsers ##
 echo '###Installing Browsers..'
+
+echo '###Installing Chrome..'
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt update
 sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+echo '###Installing Brave Browser..'
+sudo apt install apt-transport-https curl
+
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+sudo apt update
+
+sudo apt install brave-browser
 
 ## Tweak Tool ##
 echo '###Installing Tweak Tool..'
